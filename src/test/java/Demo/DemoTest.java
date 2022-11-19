9ae0052f450159e351e1e451ffac67c00ba771a7
Shashikumara-bk <shashibk1095@gmail.com>
2022-11-19 12:50:04 +0530

    commit 1

--- src/test/java/Demo/DemoTest.java
+++ src/test/java/Demo/DemoTest.java
@@ -3,7 +3,7 @@ package Demo;
 public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("Hi this i WASM6 Vtiger Framework");
-		System.out.println("hiii");
+		System.out.println("hello");
 	}
 
 }
